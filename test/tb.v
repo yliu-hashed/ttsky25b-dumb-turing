7a0ee907c2efacfb1b4258312fbe94a9a7fe38e8
YLIU <72162739+yliu-hashed@users.noreply.github.com>
2025-10-30 17:39:14 -0400

    Add a basic test case

--- test/tb.v
+++ test/tb.v
@@ -6,19 +6,13 @@
 */
 module tb ();
 
-  // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
-  initial begin
-    $dumpfile("tb.vcd");
-    $dumpvars(0, tb);
-    #1;
-  end
-
   // Wire up the inputs and outputs:
   reg clk;
   reg rst_n;
+  reg spi_rst = 0;
   reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
+  reg  [7:0] ui_in;
+  wire [7:0] uio_in;
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
@@ -27,23 +21,56 @@ module tb ();
   wire VGND = 1'b0;
 `endif
 
+  wire sio2 = 1'bZ;
+  wire holdn = 1;
+
+  M23LC512 st_ram (
+    uio_out[1],
+    uio_in[2],
+    uio_out[3],
+    uio_out[0],
+    sio2,
+    holdn,
+    spi_rst
+  );
+
+  M23LC512 tp_ram (
+    uio_out[5],
+    uio_in[6],
+    uio_out[7],
+    uio_out[4],
+    sio2,
+    holdn,
+    spi_rst
+  );
+
   // Replace tt_um_example with your module name:
   tt_um_dumb_turing_yliu_hashed user_project (
-
-      // Include power ports for the Gate Level test:
+    // Include power ports for the Gate Level test:
 `ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
+    .VPWR(VPWR),
+    .VGND(VGND),
 `endif
-
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
+    .ui_in  (ui_in),    // Dedicated inputs
+    .uo_out (uo_out),   // Dedicated outputs
+    .uio_in (uio_in),   // IOs: Input path
+    .uio_out(uio_out),  // IOs: Output path
+    .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+    .ena    (ena),      // enable - goes high when design is selected
+    .clk    (clk),      // clock
+    .rst_n  (rst_n)     // not reset
   );
+
+  // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
+  integer i;
+  initial begin
+    $dumpfile("tb.vcd");
+    $dumpvars(0, tb);
+    $readmemh("table_io.hex", st_ram.MemoryBlock);
+    for (i = 0; i < 65536; i = i + 1) begin
+      tp_ram.MemoryBlock[i] = 0;
+    end
+    #1;
+  end
 
 endmodule
--- test/test.py
+++ test/test.py
@@ -18,23 +18,20 @@ async def test_project(dut):
     dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
-    dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    await ClockCycles(dut.clk, 1)
     dut.rst_n.value = 1
 
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
+    dut.ui_in.value = 0
+    await ClockCycles(dut.clk, 400)
+    assert dut.uo_out.value == 0
+
     dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    await ClockCycles(dut.clk, 400)
+    assert dut.uo_out.value == 20
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    # assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    dut.ui_in.value = 255
+    await ClockCycles(dut.clk, 400)
+    assert dut.uo_out.value == 255
